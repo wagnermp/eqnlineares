1fffc4cc2dc2f9cf89b70a2b585bc76912b8a758
Wagner Mariano <wmariano@id.uff.br>
2017-11-07 15:47:48 -0200

    atualizando codigo

---
+++ exemplo1.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#define N 3
+
+double f1(double x[N]) 
+{
+	 return cos(x[1]*x[2])/3.+(1./6.);
+}
+
+double f2(double x[N])
+{
+	 return sqrt((x[0]*x[0])+sin(x[2])+1.06)/9-0.1; 
+}
+
+double f3(double x[N])
+{
+	 return -(1./20)*exp(-x[0]*x[1])-(10*M_PI-3)/60; 
+}
+
+int main(int argc, char **argv)
+{
+	double x[N]={0}, xa[N]={1, -1, 1};
+	
+	double eps, tol, norm, norma;
+	
+	double (*equacao[N])()=(f1,f2,f3);
+	
+	int i;
+
+	for(i=0; i<N; i++)
+	{
+		x[i]=equacao[i](xa);
+	}
+	
+	for(i=0; i<N; i++)
+	{
+		printf("x[%d]= %lf \n ", i, x[i]);
+	}
+}
+
